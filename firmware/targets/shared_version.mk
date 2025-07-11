a2f3b09701f95b7d1b49351102c20001bdf4976e
Larry Ruckman <ruckman@slac.stanford.edu>
2025-07-11 16:35:12 -0700

    upgrading to submodules/axi-soc-ultra-plus-core@6.0.0

--- firmware/targets/shared_version.mk
+++ firmware/targets/shared_version.mk
@@ -1,10 +1,10 @@
-# Define Firmware Version: v2.1.0.0
-export PRJ_VERSION = 0x02010000
+# Define Firmware Version: v3.0.0.0
+export PRJ_VERSION = 0x03000000
 
 # Include .XSA in image dir
 export GEN_XSA_IMAGE = 1
 
 # Define release
 ifndef RELEASE
-export RELEASE = KiraKv260
+export RELEASE = all
 endif
